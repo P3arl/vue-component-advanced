c1077eee1f236ec265a943dfb6f901ba71cd1477
Parveen Parmar <pparmar@wsgc.com>
2020-03-28 19:27:31 +0530

    passing content without slots

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,23 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               
+               <app-quote quote="A wonderful quote!"></app-quote>
+               <!-- <app-quote>
+                   <h2>The Quote</h2>
+                   <p>A Wonderful Quote!</p>
+               </app-quote> -->
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import Quote from './components/Quote.vue'
 
     export default {
-        
+        components: {
+            appQuote: Quote
+        }
     }
 </script>
 
